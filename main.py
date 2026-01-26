72ccfca972cfd878b3503da16a567515331c619d
Vapesmoker 420 <notyourbisensss@gmail.com>
2026-01-27 00:35:35 +0200

    opencv working

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world! its initial!")
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+feed = cv.VideoCapture(0)
+if not feed.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
+while True:
+    ret, frame = feed.read()
+    
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+    cv.imshow("Webcam Feed", frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+feed.release()
+cv.destroyAllWindows()
