3010b3c24f22c49246f4f033e5c333b5b60e80a9
Vapesmoker 420 <notyourbisensss@gmail.com>
2026-01-27 20:06:14 +0200

    small debug info

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import tomllib
 import capture.camera as camera
 import output.preview as preview
 import logging
+import output.debug_overlay as debug_overlay
+from time import perf_counter
 
 with open("config.toml", "rb") as f:
     config = tomllib.load(f)
@@ -22,19 +24,35 @@ debug = config["output"]["debug"]
 
 capture = camera.open_camera(cam_id, w, h, fps)
 try: # main loop
+    t_prev = perf_counter()
+    fps_smooth = 0.0
     logging.debug("main loop started")    
     while working:
+        
         frame_bgr, timestamp = camera.read_frame(capture)
+        
         if frame_bgr is None:
             print("Failed to read frame from camera")
             break
+        
+        if debug:
+            t = perf_counter()
+            dt = t - t_prev
+            t_prev = t
+            fps_instant = 1.0 / dt if dt > 0 else 0
+            fps_smooth = 0.9 * fps_smooth + 0.1 * fps_instant
+            
+            actual_w = capture.get(cv.CAP_PROP_FRAME_WIDTH)
+            actual_h = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
+            fps_reported = capture.get(cv.CAP_PROP_FPS)
+            frame_bgr = debug_overlay.draw_dbg_frameinfo(frame_bgr, timestamp, w, h, fps, cam_id, fps_smooth, actual_w, actual_h, fps_reported)
+            
         preview.open_preview(frame_bgr)
         
         if preview.should_close():
             break
+        
 finally:
     working = False
     camera.release(capture)
     cv.destroyAllWindows()
-        
-        
---
+++ output/debug_overlay.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import logging
+
+module_name = "DEBUG"
+logging.getLogger(__name__)
+
+DEBUG_FONT = cv.FONT_HERSHEY_SIMPLEX
+DEBUG_FONT_SCALE = 0.6
+DEBUG_FONT_COLOR = (0, 255, 0)
+DEBUG_FONT_THICKNESS = 1
+
+def draw_dbg_frameinfo(frame_bgr, timestamp, w, h, fps, cam_id, fps_smooth, actual_w, actual_h, fps_reported):
+    config_info = f"Config:{w}x{h}, fps:{fps}, cam_id:{cam_id}"
+    actual_info = f"Actual:{int(actual_w)}x{int(actual_h)}, FPS reported:{fps_reported:.2f}"
+    fps_info = f"FPS calculated:{fps_smooth:.2f}"
+    timestamp_info = f"Timestamp:{timestamp}ms"
+    lines = [config_info, actual_info, fps_info, timestamp_info]
+    y0, dy = 20, 25
+    for i, line in enumerate(lines):
+        y = y0 + i * dy
+        cv.putText(frame_bgr, line, (10, y), DEBUG_FONT, DEBUG_FONT_SCALE, DEBUG_FONT_COLOR, DEBUG_FONT_THICKNESS, cv.LINE_AA)
+    
+    return frame_bgr
